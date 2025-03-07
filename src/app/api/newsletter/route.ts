a2f1876eb5d556f6469440bb2ce953d722f5e302
Amogh Allani <amogh.allani@outlook.com>
2025-03-07 14:24:22 -0500

    Newsletter stuff, error 500

---
+++ src/app/api/newsletter/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { db } from "@/db/drizzle";
+import { subscribers } from "@/db/schemas/newsletter";
+import { eq } from "drizzle-orm";
+export async function GET(){
+    return NextResponse.json({message : "Hello?"});
+}
+
+export async function POST(req: Request) {
+    try {
+        const { email, subscribed } = await req.json();
+
+        if (!email) {
+            return NextResponse.json({ message: "Email is required" }, { status: 400 });
+        }
+        if (subscribed) {
+            await db.delete(subscribers).where(eq(subscribers.email, email));
+            return NextResponse.json({ message: "unsubscribed" });
+        } else {
+            await db.insert(subscribers).values({ email, subscribed: true }).onConflictDoNothing();
+            return NextResponse.json({ message: "subscribed" });
+        }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: `Error` }, { status: 500 });
+    }
+}
