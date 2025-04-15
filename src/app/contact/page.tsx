173a0acc94c220f56a8e16b32b5e33542ae22b43
Amogh Allani <amogh.allani@outlook.com>
2025-04-14 22:53:25 -0400

    Contact update, schemas update (contact)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,14 @@ import { Button } from "@/components/ui/button"
 const Contact = () => {
     return(
         <div className="p-8 flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold mb-4 ">Contact Us!</h1>
-            <textarea placeholder="Enter your question" className="w-full lg:w-[600px] mt-6 h-40 py-4 px-6 text-base resize-none border-4 border-gray-300 rounded-lg"/>
+            <h1 className="text-3xl font-bold mb-4">Contact Us!</h1>       
+            <input 
+                type="email" 
+                placeholder="Enter your email" 
+                className="w-full lg:w-[600px] mt-4 py-4 px-6 text-base border-4 border-gray-300 rounded-lg"/>
+            <textarea 
+                placeholder="Enter your question" 
+                className="w-full lg:w-[600px] mt-6 h-40 py-4 px-6 text-base resize-none border-4 border-gray-300 rounded-lg" />
             <Button className="text-white mt-6 w-full lg:w-[600px]">Submit</Button> 
         </div>
     );
