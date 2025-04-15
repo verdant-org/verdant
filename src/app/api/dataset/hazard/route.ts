9f6ecf1399630e6a95ad5686460c16ab9f77f3ee
carlo fraley <carlo.fraley@ufl.edu>
2025-04-14 21:57:37 -0400

    Dashboard Url updates with location, share button gets link, but link only updates side panel with data so far, need to fix so map gets updated as well

--- src/app/api/dataset/hazard/route.ts
+++ src/app/api/dataset/hazard/route.ts
@@ -13,3 +13,28 @@ export async function POST(request: NextRequest) {
     
     return NextResponse.json(countyData)
 }
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const fips = searchParams.get("fips")
+
+    if (!fips) {
+        return NextResponse.json(
+            { error: "Missing FIPS code" },
+            { status: 400 }
+        )
+    }
+
+    const countyData = await db.query.hazard.findFirst({
+        where: eq(hazard.stateCountyFipsCode, fips),
+    })
+
+    if (!countyData) {
+        return NextResponse.json(
+            { error: "County not found" },
+            { status: 404 }
+        ) 
+    }
+    
+    return NextResponse.json(countyData)
+}
